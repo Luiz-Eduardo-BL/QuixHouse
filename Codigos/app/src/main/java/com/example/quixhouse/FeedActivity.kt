97b4d68ce323e3b51e5a1b683c997051e4984ddf
Hugo Santos da Costa Bessa <hugobessa@alu.ufc.br>
2023-09-09 23:16:25 -0300

    feat: Added Bottom Navigation for Navigation Between Fragments - Implemented Bottom Navigation for navigating between HomeFragment, ProfileFragment, and SettingsFragment - Updated layout and logic in FeedActivity to handle fragment navigation

--- Codigos/app/src/main/java/com/example/quixhouse/FeedActivity.kt
+++ Codigos/app/src/main/java/com/example/quixhouse/FeedActivity.kt
@@ -1,43 +1,45 @@
 package com.example.quixhouse
 
-import android.media.Image
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import android.widget.ImageView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.quixhouse.databinding.ActivityFeedBinding
-import com.google.android.material.navigation.NavigationBarView
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFeedBinding
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(HomeFragment())
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home -> replaceFragment(HomeFragment())
-                R.id.profile -> replaceFragment(ProfileFragment())
-                R.id.settings -> replaceFragment(SettingsFragment())
-            }
-            true
-        }
+        initNavigation()
+
+//        replaceFragment(HomeFragment())
+//
+//        binding.bottomNavigationView.setOnItemSelectedListener {
+//            when(it.itemId){
+//                R.id.home -> replaceFragment(HomeFragment())
+//                R.id.profile -> replaceFragment(ProfileFragment())
+//                R.id.settings -> replaceFragment(SettingsFragment())
+//            }
+//            true
+//        }
 
     }
 
-    private fun replaceFragment(fragment: androidx.fragment.app.Fragment) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
+
+//    private fun replaceFragment(fragment: androidx.fragment.app.Fragment) {
+//        val fragmentTransaction = supportFragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragment_container, fragment)
+//        fragmentTransaction.commit()
+//    }
 }
