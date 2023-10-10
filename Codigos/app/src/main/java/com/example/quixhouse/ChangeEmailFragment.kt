592e99c285a6015b6b5a25d51f7096561f281eb8
Luiz-Eduardo-BL <limadudu34@gmail.com>
2023-10-10 11:26:58 -0300

    feat: updated icons, splash screen, and app icon
    - Updated app icons for various screen sizes and resolutions
    - Improved the splash screen design and appearance
    - Replaced the app's launcher icon with the updated version

--- Codigos/app/src/main/java/com/example/quixhouse/ChangeEmailFragment.kt
+++ Codigos/app/src/main/java/com/example/quixhouse/ChangeEmailFragment.kt
@@ -15,5 +15,4 @@ class ChangeEmailFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_change_password, container, false)
     }
-
 }
