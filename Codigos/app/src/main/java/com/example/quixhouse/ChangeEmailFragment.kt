9cf79b1ee052780aed622ed5d970d590fd9cb3e9
Luiz-Eduardo-BL <limadudu34@gmail.com>
2023-10-16 15:01:51 -0300

    feat: updated ChangeEmailFragment and ChangePasswordFragment
    - Updated ChangeEmailFragment to change the user's email in the database and in real-time authentication
    - Updated ChangePasswordFragment to change the user's password in Authentication
    - Removed password fields from ChangeEmailFragment.xml

--- Codigos/app/src/main/java/com/example/quixhouse/ChangeEmailFragment.kt
+++ Codigos/app/src/main/java/com/example/quixhouse/ChangeEmailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
@@ -26,18 +27,48 @@ class ChangeEmailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val user = FirebaseAuth.getInstance().currentUser
         val database = FirebaseDatabase.getInstance().reference
         val uid = user?.uid
-        val email = database.child("users").child(uid.toString()).child("email")
-        email.get().addOnSuccessListener {
-            val editEmail = view.findViewById<EditText>(R.id.editNewEmail)
-            editEmail.hint = it.value.toString()
+        val userEmail = database.child("users").child(uid.toString()).child("email")
+        userEmail.get().addOnSuccessListener {
+            view.findViewById<EditText>(R.id.editNewEmail).hint = it.value.toString()
         }.addOnFailureListener{
             println("Failed to read value.")
         }
-
+        if (user != null) {
+            view.findViewById<Button>(R.id.buttonSendEmail).setOnClickListener {
+                changeEmail(view)
+            }
+        }
     }
+
+    fun changeEmail(view: View) {
+        val user = FirebaseAuth.getInstance().currentUser
+        val email = view.findViewById<EditText>(R.id.editNewEmail).text.toString()
+        if (user != null) {
+            user.updateEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            context,
+                            "Email alterado com sucesso",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        // Atualizar o email no realtime database
+                        val database = FirebaseDatabase.getInstance()
+                        val myRef = database.getReference("users")
+                        myRef.child(user.uid).child("email").setValue(email)
+                        findNavController().navigate(R.id.perfilFragment)
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Email invalido",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+   }
 
 }
--- Codigos/app/src/main/java/com/example/quixhouse/ChangePasswordFragment.kt
+++ Codigos/app/src/main/java/com/example/quixhouse/ChangePasswordFragment.kt
@@ -1,10 +1,13 @@
 package com.example.quixhouse
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ChangePasswordFragment : Fragment() {
@@ -15,5 +18,56 @@ class ChangePasswordFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_change_password, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        verifyAuthentication()
+        view.findViewById<com.google.android.material.button.MaterialButton>(R.id.buttonSendPassword).setOnClickListener {
+            changePassword(view)
+        }
+    }
+
+    private fun verifyAuthentication() {
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+    }
+
+    private fun validateData(): Boolean {
+        val newPassword = view?.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.editNewPassword)?.text.toString()
+        val confirmNewPassword = view?.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.confirmNewPassword)?.text.toString()
+        return if (newPassword.isNotEmpty() && confirmNewPassword.isNotEmpty()) {
+            if (newPassword == confirmNewPassword) {
+                true
+            } else {
+                Toast.makeText(context, "As senhas não são iguais", Toast.LENGTH_SHORT).show()
+                false
+            }
+        } else {
+            Toast.makeText(context, "Preencha os campos", Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    fun changePassword(view: View) {
+        if (validateData()) {
+            val newPassword = view.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.editNewPassword).text.toString()
+            FirebaseAuth.getInstance().currentUser?.updatePassword(newPassword)?.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "Senha alterada com sucesso", Toast.LENGTH_SHORT).show()
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(activity, MainActivity::class.java)
+                    startActivity(intent)
+                    activity?.finish()
+
+                } else {
+                    Toast.makeText(context, "Erro ao alterar senha", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 
 }
